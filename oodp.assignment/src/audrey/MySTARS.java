0b113d8d0299a1b6cd3e5c74cf4669298db0a48d
AUDR0012 <AUDR0012@e.ntu.edu.sg>
2017-04-06 00:21:07 +0800

    .

--- oodp.assignment/src/audrey/MySTARS.java
+++ oodp.assignment/src/audrey/MySTARS.java
@@ -21,6 +21,9 @@ public class MySTARS {
 		ArrayList<Student> studentList = new ArrayList<Student>();
 		ArrayList<Course> courseList = new ArrayList<Course>();
 
+		Notifier notify = new Notifier();
+
+		Logger logger = null;
 		Comparable user = null, peer = null;
 		Course course = null;
 		Group group = null, group2 = null;
@@ -29,16 +32,13 @@ public class MySTARS {
 		String in_string;
 		boolean found;
 
-		// System.out.print("add data = 1");												// TEMPORARY
-		// if (in.nextInt() == 1)														// TEMPORARY
-		// 	Developer.addData(userList, studentList, courseList);						// TEMPORARY
+		// Developer.addData(userList, studentList, courseList);
 
 		Menu.printHeader(user);
 		readData(userList, studentList, courseList);
 
-		// System.out.print("login = 1"); 													// TEMPORARY
-		// user = (in.nextInt() == 1) ? login(in, userList) : userList.get(3).getUser();	// TEMPORARY
-		user = login(in, userList);
+		logger = login(userList);
+		user = logger.getUser();
 
 		if (user instanceof Admin)
 		{
@@ -53,73 +53,78 @@ public class MySTARS {
 						Date start, end;
 
 						System.out.println("Enter the Start Date of Access Time");
-						start = FormatString.enterDateTime(in, "date_time");
+						start = Formatter.enterDateTime(in, "date_time");
 
 						System.out.println("Enter End Date of Access Time");
-						end = FormatString.enterDateTime(in, "date_time");
+						end = Formatter.enterDateTime(in, "date_time");
 
 						for (Logger lo : userList)
 						{
 							lo.setAccessSTime(start);
 							lo.setAccessETime(end);
 						}
-						System.out.println("Access Time has been updated to " + FormatString.getTimePeriod(start, end, "dd-MM-yy hh:mm"));
+						System.out.println("Access Time has been updated to " + Formatter.getTimePeriod(start, end, "dd-MM-yy hh:mm"));
 						break;
 					}
 					case 2:
 					{ // Add a Student
-						Logger lo;
-						Student st;
+						Logger newLogger = new Logger();
+						Student newStudent = new Student();
 
-						lo = new Logger();
 						// Username
 						System.out.print("Enter Username: ");
-						lo.setUsername(in.next());
+						newLogger.setUsername(in.next());
 						// Password
 						System.out.print("Enter Password: ");
-						lo.setPassword(in.next());
+						newLogger.setPassword(in.next());
 
-						st = new Student();
 						// Name
 						System.out.print("Enter Name: ");
 						in.nextLine();
-						st.setName(in.nextLine());
+						newStudent.setName(in.nextLine());
 						// Gender
 						Enumerator.printAll(Gender.class);
-						st.setGender(Enumerator.nextEnum(Gender.class, in));
+						newStudent.setGender(Enumerator.nextEnum(Gender.class, in));
 						// Matric Number
 						System.out.print("Enter Matric Number: ");
-						st.setMatricNo(in.next());
+						newStudent.setMatricNo(in.next());
 						// Study Year
 						System.out.print("Enter Study Year: ");
-						st.setStudyYear(in.nextInt());
+						newStudent.setStudyYear(in.nextInt());
 						// Email
-						System.out.print("Enter Email: ");
-						st.setEmail(in.next());
+						do
+						{
+							if (newStudent.getEmail() != null)
+							{
+								System.out.println("Please enter a valid Email Address.");
+							}
+							System.out.print("Enter Email Address: ");
+							newStudent.setEmail(in.next());
+						} while (!Formatter.emailValid(newStudent.getEmail()));
 						// Date of Birth
 						System.out.print("Enter Date of Birth: ");
-						st.setDob(FormatString.enterDateTime(in, "date"));
+						newStudent.setDob(Formatter.enterDateTime(in, "date"));
 						// Nationality
 						System.out.print("Enter Nationality: ");
-						st.setNationality(in.next());
+						newStudent.setNationality(in.next());
 						// Phone Number
 						do
 						{
-							if (st.getPhoneNo() != null)
+							if (newStudent.getPhoneNo() != null)
 							{
 								System.out.println("Please enter a valid Local Phone Number.");
 							}
 							System.out.print("Enter Phone Number: ");
-							st.setPhoneNo(in.next());
-						} while (st.getPhoneNo().length() != 8);
+							newStudent.setPhoneNo(in.next());
+						} while (newStudent.getPhoneNo().length() != 8);
 						// Notification
 						Enumerator.printAll(Notification_Status.class);
-						st.setNotification(Enumerator.nextEnum(Notification_Status.class, in));
+						newStudent.setNotification(Enumerator.nextEnum(Notification_Status.class, in));
 
-						lo.setUser(st);
+						newLogger.setUser(newStudent);
 
-						studentList.add(st);
-						userList.add(lo);
+						studentList.add(newStudent);
+						userList.add(newLogger);
 						break;
 					}
 					case 3:
@@ -177,10 +182,10 @@ public class MySTARS {
 								session.setDay(Enumerator.nextEnum(Day.class, in));
 								// Start Time
 								System.out.print("Enter Start Time: ");
-								session.setSTime(FormatString.enterDateTime(in, "date"));
+								session.setSTime(Formatter.enterDateTime(in, "date"));
 								// Hours
 								System.out.print("Enter Number of Hours: ");
-								session.setETime(FormatString.addHours(session.getSTime(), in.nextInt()));
+								session.setETime(Formatter.addHours(session.getSTime(), in.nextInt()));
 								// Venue
 								System.out.print("Enter Venue: ");
 								in.nextLine();
@@ -261,8 +266,6 @@ public class MySTARS {
 					}
 					case 0:
 					{ // Exit
-						// System.out.print("write = 1");									// TEMPORARY
-						// if (in.nextInt() == 1)											// TEMPORARY
 						writeData(userList, courseList);
 						System.out.println("Exiting.");
 						break;
@@ -296,7 +299,7 @@ public class MySTARS {
 								course = group.getCourse(courseList);
 								if (!course.findStudent(student))
 								{
-									System.out.println(FormatString.tabs(40, "", "Index Number: " + group.getIndexNo()) + "Course: " + course.getId());
+									System.out.println(Formatter.tabs(40, "", "Index Number: " + group.getIndexNo()) + "Course: " + course.getId());
 									course.printGroup(group, PARSE_LENGTH, PARSE_DELIMITER);
 									System.out.println("Course Type: " + course.getType());
 
@@ -339,14 +342,18 @@ public class MySTARS {
 								group = student.getCourseGroups().get(in_int - 1);
 								course = group.getCourse(courseList);
 
-								System.out.println(FormatString.tabs(40, "", "Index Number: " + group.getIndexNo()) + "Course: " + course.getId());
+								System.out.println(Formatter.tabs(40, "", "Index Number: " + group.getIndexNo()) + "Course: " + course.getId());
 								course.printGroup(group, PARSE_LENGTH, PARSE_DELIMITER);
-								System.out.println(FormatString.tabs(50, "", "Course Type: " + course.getType()) + "Status: " + group.findStudent(student, "status"));
+								System.out.println(Formatter.tabs(50, "", "Course Type: " + course.getType()) + "Status: " + group.findStudent(student, "status"));
 
 								System.out.print("Confirm to Add Course? (Y/N) ");
 								if (in.next().equalsIgnoreCase("Y"))
 								{
-									group.dropStudentFromGroup(student);
+									if (group.dropStudentFromGroup(student))
+									{
+										group.updateWaitlist(student, course, notify);
+									}
+
 									student.getCourseGroups().remove(group);
 								}
 								else
@@ -386,9 +393,9 @@ public class MySTARS {
 							if (!Objects.equals(null, (group = groupExist(courseList, in_int))))
 							{
 								course = group.getCourse(courseList);
-								System.out.println(FormatString.tabs(40, "", "Index Number: " + group.getIndexNo()) + "Course: " + course.getId());
+								System.out.println(Formatter.tabs(40, "", "Index Number: " + group.getIndexNo()) + "Course: " + course.getId());
 								course.printGroup(group, PARSE_LENGTH, PARSE_DELIMITER);
-								System.out.println(FormatString.tabs(50, "", "Places Available: " + group.getVacancy())
+								System.out.println(Formatter.tabs(50, "", "Places Available: " + group.getVacancy())
 										+ "Length of WaitList: " + group.getWaitlist().size());
 								break;
 							}
@@ -423,10 +430,10 @@ public class MySTARS {
 											if (!Objects.equals(null, (group = student.isOverlap(group2))))
 											{
 												System.out.println("Subject: " + course.getId());
-												System.out.println(FormatString.tabs(100, "", "Current Index Number: " + String.valueOf(group.getIndexNo()))
+												System.out.println(Formatter.tabs(100, "", "Current Index Number: " + String.valueOf(group.getIndexNo()))
 														+ "New Index Number: " + String.valueOf(group2.getIndexNo()));
 												course.print2Groups(group, group2, PARSE_LENGTH, PARSE_DELIMITER);
-												System.out.println(FormatString.tabs(100, "", "Subject Type: " + course.getType())
+												System.out.println(Formatter.tabs(100, "", "Subject Type: " + course.getType())
 														+ "Status: " + Enumerator.string(Group_Status.REGISTERED));
 												System.out.print("Confirm to Change Index Number? (Y/N) ");
 												if (in.next().equalsIgnoreCase("Y"))
@@ -496,7 +503,7 @@ public class MySTARS {
 							if (Enumerator.string(Group_Status.REGISTERED).equals(group.findStudent(student, "status")))
 							{
 								System.out.println("Please ask your Peer to Log in");
-								if (!Objects.equals(null, (peer = login(in, userList)))
+								if (!Objects.equals(null, (peer = login(userList).getUser()))
 										&& peer instanceof Student)
 								{
 									System.out.print("Enter Peer's Index Number: ");
@@ -512,12 +519,12 @@ public class MySTARS {
 														!Objects.equals(null, (group2 = ((Student) peer).isOverlap(group))))
 												{
 													System.out.println("Subject: " + course.getId());
-													System.out.println(FormatString.tabs(100, "", "Student #1 ")
+													System.out.println(Formatter.tabs(100, "", "Student #1 ")
 															+ "Student #2");
-													System.out.println(FormatString.tabs(100, "", "Matric Number: " + student.getMatricNo() + " Index Number: " + group.getIndexNo())
+													System.out.println(Formatter.tabs(100, "", "Matric Number: " + student.getMatricNo() + " Index Number: " + group.getIndexNo())
 															+ "Matric Number: " + ((Student) peer).getMatricNo() + " Index Number: " + group2.getIndexNo());
 													course.print2Groups(group, group2, PARSE_LENGTH, PARSE_DELIMITER);
-													System.out.println(FormatString.tabs(100, "", "Subject Type: " + course.getType())
+													System.out.println(Formatter.tabs(100, "", "Subject Type: " + course.getType())
 															+ "Subject Type: " + course.getType());
 													System.out.print("Confirm to Swop Index Number? (Y/N) ");
 													if (in.next().equalsIgnoreCase("Y"))
@@ -612,7 +619,7 @@ public class MySTARS {
 								System.out.println("Current Email:" + student.getEmail());
 								System.out.print("Enter the new Email:");
 								in_string = in.next();
-								if (FormatString.emailValid(in_string))
+								if (Formatter.emailValid(in_string))
 								{
 									student.setEmail(in_string);
 								}
@@ -646,7 +653,16 @@ public class MySTARS {
 							}
 							case 4:
 							{ // Password
-								System.out.println("Yet to Develop!"); //TODO:
+								System.out.print("Enter your current Password: ");
+								if (logger.getPassword().equals(inputPassword()))
+								{
+									System.out.print("Enter your new Password: ");
+									logger.setPassword(inputPassword());
+								}
+								else
+								{
+									System.out.println("Current Password entered incorrectly.");
+								}
 								break;
 							}
 							case 0:
@@ -663,8 +679,6 @@ public class MySTARS {
 					}
 					case 0:
 					{ // Exit
-						// System.out.print("write = 1");									// TEMPORARY
-						// if (in.nextInt() == 1)											// TEMPORARY
 						writeData(userList, courseList);
 						System.out.println("Exiting.");
 						break;
@@ -683,40 +697,44 @@ public class MySTARS {
 		}
 	}
 
-	public static Comparable login(Scanner in, ArrayList<Logger> userList)
+	public static String inputPassword()
 	{
-		Console console = System.console();
+		//return Formatter.hashPassword(String.copyValueOf(System.console().readPassword()));
+		return Formatter.hashPassword((new Scanner(System.in)).next());
+	}
+
+	public static Logger login(ArrayList<Logger> userList)
+	{
 		String username, password;
 
 		System.out.print("Username: ");
-		username = in.next();
+		username = (new Scanner(System.in)).next();
 		System.out.print("Password: ");
-		password = in.next();															// TEMPORARY
-		//password = String.copyValueOf(console.readPassword());
+		password = inputPassword();
 
-		for (Logger l : userList)
+		for (Logger lo : userList)
 		{
-			if (l.getUsername().equalsIgnoreCase(username))
+			if (lo.getUsername().equalsIgnoreCase(username))
 			{
-				if (l.getPassword().equals(FormatString.hashPassword(password)))
+				if (lo.getPassword().equals(password))
 				{
-					if (l.getUser() instanceof Student)
+					if (lo.getUser() instanceof Student)
 					{
 						Date today = new Date();
-						if (today.after(l.getAccessSTime()) && today.before(l.getAccessETime()))
+						if (today.after(lo.getAccessSTime()) && today.before(lo.getAccessETime()))
 						{
-							return l.getUser();
+							return lo;
 						}
 						else
 						{
-							System.out.println("You can only access between " + FormatString.getString(l.getAccessSTime(), "dd-MM-yyyy hh:mm a")
-									+ " to " + FormatString.getString(l.getAccessETime(), "dd-MM-yyyy hh:mm a"));
+							System.out.println("You can only access between " + Formatter.getString(lo.getAccessSTime(), "dd-MM-yyyy hh:mm a")
+									+ " to " + Formatter.getString(lo.getAccessETime(), "dd-MM-yyyy hh:mm a"));
 							return null;
 						}
 					}
 					else
 					{
-						return l.getUser();
+						return lo;
 					}
 				}
 			}
@@ -737,7 +755,6 @@ public class MySTARS {
 			{
 				studentList.add((Student) l.getUser());
 			}
-			
 		}
 
 		// Course
